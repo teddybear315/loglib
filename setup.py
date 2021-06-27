67015d7dc83bb4409e81eafe81927dcf213f738f
Logan Houston <44184086+teddybear315@users.noreply.github.com>
2021-06-27 18:05:53 -0500

    dist

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='loglib',
+    version='1.0.0',
+    description='A decent logging system with some settings built in',
+    url='https://github.com/shuds13/pyexample',
+    author='Logan Houston',
+    author_email='logan.houston4509@gmail.com',
+    license='Free Domain',
+    packages=['loglib'],
+    install_requires=['neotermcolor>=2.0'],
+    classifiers=[
+        'Programming Language :: Python :: 3.9'
+    ],
+)
